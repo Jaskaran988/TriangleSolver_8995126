f412cc0dc44efaf0b19ac9e875dc3e30b6a1646c
Jaskaran <jaskarandeepsingh9888@gmail.com>
2024-10-10 22:14:46 -0400

    Created Test Cases for Valid Isosceles Triangle

--- TriangleTest/Class1.cs
+++ TriangleTest/Class1.cs
@@ -14,7 +14,7 @@ namespace TriangleTest
     [TestFixture]
     public class TriangleTests
     {
-        // Valid Equilateral Triangle
+        // 1.Valid Equilateral Triangle
         [Test]
         public void AnalyzeTriangle_EquilateralTriangle_ShouldReturnEquilateral() { 
         
@@ -25,6 +25,41 @@ namespace TriangleTest
 
             string result = Triangle.AnalyzeTriangle(side1, side2, side3);
 
+            Assert.That(result, Is.EqualTo(expected));
+        }
+        // 2. Valid Isosceles Triangle (case 1)
+        [Test]
+        public void AnalyzeTriangle_Isosceles_ShouldReturnIsosceles_Case1()
+        {
+            int side1 = 5;
+            int side2 = 5;
+            int side3 = 3;
+            string expected = "Isosceles triangle";
+            string result = Triangle.AnalyzeTriangle(side1, side2, side3);
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        // 3. Valid Isosceles Triangle (case 2)
+        [Test]
+        public void AnalyzeTriangle_Isosceles_ShouldReturnIsosceles_Case2()
+        {
+            int side1 = 6;
+            int side2 = 6;
+            int side3 = 4;
+            string expected = "Isosceles triangle";
+            string result = Triangle.AnalyzeTriangle(side1, side2, side3);
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        // 4. Valid Isosceles Triangle (case 3)
+        [Test]
+        public void AnalyzeTriangle_Isosceles_ShouldReturnIsosceles_Case3()
+        {
+            int side1 = 10;
+            int side2 = 10;
+            int side3 = 7;
+            string expected = "Isosceles triangle";
+            string result = Triangle.AnalyzeTriangle(side1, side2, side3);
             Assert.That(result, Is.EqualTo(expected));
         }
     }
