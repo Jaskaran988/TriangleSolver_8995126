47a4957fee5297756cd2668057f9d6d3b8653d68
Jaskaran <jaskarandeepsingh9888@gmail.com>
2024-10-10 22:09:40 -0400

    Created Test Case for Equilateral Triangle

--- TriangleTest/Class1.cs
+++ TriangleTest/Class1.cs
@@ -11,4 +11,21 @@ using TriangleSolver;
 
 namespace TriangleTest
 {
-}
+    [TestFixture]
+    public class TriangleTests
+    {
+        // Valid Equilateral Triangle
+        [Test]
+        public void AnalyzeTriangle_EquilateralTriangle_ShouldReturnEquilateral() { 
+        
+            int side1 = 13;
+            int side2 = 13;
+            int side3 = 13;
+            string expected = "Equilateral triangle";
+
+            string result = Triangle.AnalyzeTriangle(side1, side2, side3);
+
+            Assert.That(result, Is.EqualTo(expected));
+        }
+    }
+    }
