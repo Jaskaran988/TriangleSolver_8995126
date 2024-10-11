1e2acf335e45dc72dabf96e21e6f4a178269b119
Jaskaran <jaskarandeepsingh9888@gmail.com>
2024-10-11 17:01:30 -0400

    Created 5 test cases for valid Scalene Triangle

--- TriangleTest/Class1.cs
+++ TriangleTest/Class1.cs
@@ -59,6 +59,64 @@ namespace TriangleTest
             string result = Triangle.AnalyzeTriangle(side1, side2, side3);
             Assert.That(result, Is.EqualTo(expected));
         }
+        // 5. Valid Scalene Triangle (case 1)
+        [Test]
+        public void ValidTriangle_Input14and13and18_OutputScaleneTriangle_Case1()
+        {
+            int side1 = 14;
+            int side2 = 13;
+            int side3 = 18;
+            string expected = "Scalene triangle";
+            string result = Triangle.AnalyzeTriangle(side1, side2, side3);
+            Assert.That(result, Is.EqualTo(expected));
+        }
 
+        // 6. Valid Scalene Triangle (case 2)
+        [Test]
+        public void ValidTriangle_Input30and28and23_OutputScaleneTriangle_Case2()
+        {
+            int side1 = 30;
+            int side2 = 28;
+            int side3 = 23;
+            string expected = "Scalene triangle";
+            string result = Triangle.AnalyzeTriangle(side1, side2, side3);
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        // 7. Valid Scalene Triangle (case 3)
+        [Test]
+        public void ValidTriangle_Input23and32and21_OutputScaleneTriangle_Case3()
+        {
+            int side1 = 23;
+            int side2 = 32;
+            int side3 = 21;
+            string expected = "Scalene triangle";
+            string result = Triangle.AnalyzeTriangle(side1, side2, side3);
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        // 8. Valid Scalene Triangle (case 4)
+        [Test]
+        public void ValidTriangle_Input45and39and35_OutputScaleneTriangle_Case4()
+        {
+            int side1 = 45;
+            int side2 = 39;
+            int side3 = 35;
+            string expected = "Scalene triangle";
+            string result = Triangle.AnalyzeTriangle(side1, side2, side3);
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        // 9. Valid Scalene Triangle (case 5)
+        [Test]
+        public void ValidTriangle_Input32and23and44_OutputScaleneTriangle_Case5()
+        {
+            int side1 = 32;
+            int side2 = 23;
+            int side3 = 44;
+            string expected = "Scalene triangle";
+            string result = Triangle.AnalyzeTriangle(side1, side2, side3);
+            Assert.That(result, Is.EqualTo(expected));
+        }
     }
 }
